c2a0233d69e3020dea1c2876f814cbff6a80674c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 10:40:09 +0300

    Made some changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 
 export default function TabOneScreen() {
 	useEffect(() => {
-		console.log(fetchTopRatedMovies())
+		fetchTopRatedMovies()
 	}, [])
 
 	return <View style={styles.container}></View>
