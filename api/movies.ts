726db360f33c66f5294dbed9599c5bd6632d9249
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 10:14:13 +0300

    Made some changes

--- api/movies.ts
+++ api/movies.ts
@@ -1,5 +1,8 @@
 export const fetchTopRatedMovies = () =>
-	fetch(process.env.API_URL!)
+	fetch(
+		`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env
+			.API_KEY!}`
+	)
 		.then((res) => res.json())
 		.then((json) => console.log(json))
 		.catch((err) => console.log(err))
