f6afb4af6f8957bc9b71c16b86cd50ab427ec5b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 14:21:06 +0300

    Styled the MovieDetails page

--- app/movie/[movieId].tsx
+++ app/movie/[movieId].tsx
@@ -1,15 +1,22 @@
 import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const MovieDetailsPage = () => {
 	const { movieId } = useLocalSearchParams()
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>{movieId}</Text>
 		</View>
 	)
 }
 
 export default MovieDetailsPage
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white'
+	}
+})
