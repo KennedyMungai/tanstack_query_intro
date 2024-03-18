72bee0919eb70ddae4bbf1f85e8d8ff56dc6396f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 10:22:47 +0300

    Added api to the gitignore file to protect the ai keys:

--- api/movies.ts
+++
@@ -1,8 +0,0 @@
-export const fetchTopRatedMovies = () =>
-	fetch(
-		`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env
-			.API_KEY!}`
-	)
-		.then((res) => res.json())
-		.then((json) => console.log(json))
-		.catch((err) => console.log(err))
