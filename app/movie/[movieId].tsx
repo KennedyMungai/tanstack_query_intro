6bbf25333787a48b98e730a87f9d8a01a858cfd4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 14:41:22 +0300

    Implemented data fetching for individual movies using tanstack query

--- app/movie/[movieId].tsx
+++ app/movie/[movieId].tsx
@@ -1,13 +1,42 @@
+import { fetchSingleMovieById } from '@/api/movies'
+import { useQuery } from '@tanstack/react-query'
 import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text } from 'react-native'
 
 const MovieDetailsPage = () => {
 	const { movieId } = useLocalSearchParams()
 
+	const { data, error, isError, isPending } = useQuery({
+		queryKey: ['fetchMovieDetails', { movieId }],
+		queryFn: () => fetchSingleMovieById(movieId as string)
+	})
+
+	if (isError) {
+		return (
+			<SafeAreaView
+				style={[styles.container, { backgroundColor: 'red' }]}
+			>
+				<Text
+					style={{ fontWeight: '700', color: 'white', fontSize: 36 }}
+				>
+					{`Error: ${error.message}`}
+				</Text>
+			</SafeAreaView>
+		)
+	}
+
+	if (isPending) {
+		return (
+			<SafeAreaView style={styles.container}>
+				<ActivityIndicator style={{ flex: 1 }} />
+			</SafeAreaView>
+		)
+	}
+
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text>{movieId}</Text>
+			<Text>{JSON.stringify(data)}</Text>
 		</SafeAreaView>
 	)
 }
