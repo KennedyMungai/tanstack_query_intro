18cdbfcc94cedc73c4e0cd8192298da6b518e8dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 12:32:40 +0300

    Styled the movie card components

--- components/MovieListItem.tsx
+++ components/MovieListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 type Props = {
 	movie: Movie
@@ -9,6 +9,25 @@ const MovieListItem = ({ movie }: Props) => {
 	return (
 		<View style={styles.cardContainer}>
 			<Text style={styles.cardTitle}>{movie.title}</Text>
+			<View
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+				<Image
+					source={{
+						uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+					}}
+					style={{
+						width: '100%',
+						aspectRatio: 1,
+						borderRadius: 5,
+						height: 'auto'
+					}}
+				/>
+			</View>
 		</View>
 	)
 }
@@ -17,14 +36,20 @@ export default MovieListItem
 
 const styles = StyleSheet.create({
 	cardContainer: {
-		width: '100%',
-		height: 200,
-		marginVertical: 5
+		width: '95%',
+		height: 'auto',
+		marginVertical: 10,
+		display: 'flex',
+		justifyContent: 'center'
 	},
 	cardTitle: {
 		fontSize: 26,
 		fontWeight: '700',
 		color: '#C1C1C1',
-		textTransform: 'uppercase'
+		textTransform: 'uppercase',
+		position: 'absolute',
+		top: 10,
+		left: 20,
+		zIndex: 10
 	}
 })
