7c720312d158ba2e5f032e84d508313680789a4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 15:08:26 +0300

    Added the ReactQueryDevTools plugin to the application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 
 import { useColorScheme } from '@/components/useColorScheme'
+import { useReactQueryDevTools } from '@dev-plugins/react-query'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const client = new QueryClient()
@@ -53,6 +54,8 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
 	const colorScheme = useColorScheme()
+
+	useReactQueryDevTools(client)
 
 	return (
 		<QueryClientProvider client={client}>
