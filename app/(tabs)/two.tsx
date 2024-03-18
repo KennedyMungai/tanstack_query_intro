bf3c1ed3d3dc8a44b3c6432aa790b4adad6385e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 16:28:46 +0300

    Updated the markup

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,63 @@
-import { StyleSheet } from 'react-native'
+import {
+	ActivityIndicator,
+	FlatList,
+	SafeAreaView,
+	StyleSheet,
+	Text
+} from 'react-native'
 
-import { View } from '@/components/Themed'
+import { fetchTopRatedMovies } from '@/api/movies'
+import MovieListItem from '@/components/MovieListItem'
+import { useQuery } from '@tanstack/react-query'
 
 export default function TabTwoScreen() {
-	return <View style={styles.container}></View>
+	const {
+		data: topRatedMoviesData,
+		error: topRatedMoviesError,
+		isError: isTopRatedMoviesError,
+		isPending: isTopRatedMoviesPending
+	} = useQuery({
+		queryKey: ['fetchWatchListMovies'],
+		queryFn: fetchTopRatedMovies
+	})
+
+	if (isTopRatedMoviesPending) {
+		return (
+			<SafeAreaView style={styles.container}>
+				<ActivityIndicator size={36} />
+			</SafeAreaView>
+		)
+	}
+
+	if (isTopRatedMoviesError) {
+		return (
+			<SafeAreaView style={styles.container}>
+				<Text
+					style={{ fontSize: 30, color: 'red', textAlign: 'center' }}
+				>{`Error - ${topRatedMoviesError.message}`}</Text>
+			</SafeAreaView>
+		)
+	}
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				data={topRatedMoviesData}
+				showsVerticalScrollIndicator={false}
+				keyExtractor={(item: Movie) => item.id.toString()}
+				renderItem={({ item }) => <MovieListItem movie={item} />}
+			/>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		backgroundColor: 'white',
+		paddingVertical: 10,
+		marginHorizontal: 10
 	}
 })
