99c5b69a6987c296aa56eaaced8db0b704e235c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 09:56:29 +0300

    Made some updates for the url fetched

--- api/movies.ts
+++ api/movies.ts
@@ -1,16 +1,5 @@
-export const fetchTopRatedMovies = () => {
-	const url = `https://api.themoviedb.org/3/movie/top_rated?page=1&api_key=${process.env.API_KEY}`
-
-	const options = {
-		method: 'GET',
-		headers: {
-			accept: 'application/json',
-			Authorization: `Bearer ${process.env.API_READ_ACCESS_TOKEN}`
-		}
-	}
-
-	fetch(url, options)
+export const fetchTopRatedMovies = () =>
+	fetch(process.env.API_URL!)
 		.then((res) => res.json())
 		.then((json) => console.log(json))
-		.catch((err) => console.error('Error ' + err))
-}
+		.catch((err) => console.log(err))
