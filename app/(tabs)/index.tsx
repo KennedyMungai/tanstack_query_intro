59c7fcc82c7e13f9b4af0fb861591a6cc65f26e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-18 11:04:27 +0300

    Added some data fetching logic and set the movies state to the list of movies fetched

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,19 @@ import { StyleSheet } from 'react-native'
 
 import { fetchTopRatedMovies } from '@/api/movies'
 import { View } from '@/components/Themed'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function TabOneScreen() {
+	const [movies, setMovies] = useState([])
+
 	useEffect(() => {
-		fetchTopRatedMovies()
+		const fetchMovies = async () => {
+			const movies = await fetchTopRatedMovies()
+
+			setMovies(movies)
+		}
+
+		fetchMovies()
 	}, [])
 
 	return <View style={styles.container}></View>
